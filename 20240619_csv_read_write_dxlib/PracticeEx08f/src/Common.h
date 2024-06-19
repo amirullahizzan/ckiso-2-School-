2b123a68f4a6118861e63daaebd8da45b1902cf2
Zan <38888951+amirullahizzan@users.noreply.github.com>
2024-06-19 12:25:01 +0900

    Add files via upload

---
+++ 20240619_csv_read_write_dxlib/PracticeEx08f/src/Common.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// 画像用構造体
+struct img
+{
+	int   ghandle = -1;	// 画像ハンドル
+	float w		= 0;	// 幅
+	float h		= 0;	// 高さ
+};
+
+// 座標用構造体
+struct pos
+{
+	float x = 0;
+	float y = 0;
+};
+
